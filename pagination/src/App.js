6b6b726330e7a37317876fc14a102e3abb3ff92d
Manjesh Gowda R <manjeshgowdar04@gmail.com>
2024-01-05 19:41:17 +0530

    MSG

---
+++ pagination/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import XPagination from "./XPagination";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <XPagination />
+    </div>
+  );
+}
+
+export default App;
